a3de64049b38d3eb3bb656330fe2500b0f0a8410
yaelle <ymarsiac@octo.com>
2020-12-10 12:31:28 +0100

    see call to the api

--- index.js
+++ index.js
@@ -47,9 +47,15 @@ const init = async () => {
       }
     });
 
+    server.events.on('response',  (request) =>{
+      console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.path + ' --> ' + request.response.statusCode);
+    }); 
+    
   await server.start();
   console.log('Server running on %s', server.info.uri);
+  
 };
+
 
 process.on('unhandledRejection', (err) => {
   console.log(err);
