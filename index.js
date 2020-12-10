da44ce5df3029cae7410679b24ffbf9a2ac510c3
yaelle <ymarsiac@octo.com>
2020-12-10 11:16:18 +0100

    add files for scalingo

--- index.js
+++ index.js
@@ -3,9 +3,10 @@
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
+  const port = parseInt(process.env.PORT, 10) || 4000;
 
   const server = Hapi.server({
-    port: 3000,
+    port: port,
     host: '0.0.0.0'
   });
 
